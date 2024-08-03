edd1c0b05f94ea1964e46f458a7fcbe7fa474dc3
Yunus Emre Şimşek <yemresimseek@gmail.com>
2024-08-03 10:12:18 +0300

    Initial Commit

---
+++ ProgrammaticNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProgrammaticNavigation
+//
+//  Created by Emre Simsek on 3.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgrammaticNavigation/ProgrammaticNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticNavigationApp.swift
+//  ProgrammaticNavigation
+//
+//  Created by Emre Simsek on 3.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
